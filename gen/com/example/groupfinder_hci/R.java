6f9a56276188a1058869c331be4624c91812187d
Shini Arora <shini.arora.258@gmail.com>
2014-11-08 19:54:26 -0500

    Sign up page added
    "Register here" button on Login opens "Sign up" page

--- gen/com/example/groupfinder_hci/R.java
+++ gen/com/example/groupfinder_hci/R.java
@@ -16,6 +16,10 @@ public final class R {
         public static final int abc_slide_out_bottom=0x7f040004;
         public static final int abc_slide_out_top=0x7f040005;
     }
+    public static final class array {
+        public static final int department_array=0x7f0c0001;
+        public static final int levelOfStudy_array=0x7f0c0000;
+    }
     public static final class attr {
         /**  Custom divider drawable to use for elements in the action bar. 
          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
@@ -985,10 +989,14 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020058;
     }
     public static final class id {
+        public static final int DepartmentLabel=0x7f050044;
+        public static final int LevelOfStudyLabel=0x7f050045;
         public static final int Login=0x7f050042;
         public static final int Password=0x7f050040;
+        public static final int PasswordLabel=0x7f050046;
         public static final int SignUp=0x7f050043;
         public static final int Unity=0x7f05003d;
+        public static final int UnityIDLabel=0x7f050047;
         public static final int UserPassword=0x7f050041;
         public static final int UserUnity=0x7f05003f;
         public static final int action_bar=0x7f05001c;
@@ -1002,7 +1010,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050045;
+        public static final int action_settings=0x7f05004d;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1013,6 +1021,8 @@ containing a value of this type.
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
+        public static final int editText1=0x7f050048;
+        public static final int editText2=0x7f050049;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
@@ -1045,6 +1055,8 @@ containing a value of this type.
         public static final int showCustom=0x7f050007;
         public static final int showHome=0x7f050004;
         public static final int showTitle=0x7f050006;
+        public static final int spinner=0x7f05004a;
+        public static final int spinner1=0x7f05004b;
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
@@ -1053,7 +1065,7 @@ containing a value of this type.
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
-        public static final int welcomtev=0x7f050044;
+        public static final int welcomtev=0x7f05004c;
         public static final int withText=0x7f05000c;
     }
     public static final class integer {
@@ -1106,12 +1118,15 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int activity_main=0x7f030017;
-        public static final int fragment_main=0x7f030018;
-        public static final int hc_main_layout=0x7f030019;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
+        public static final int activity_sign_up=0x7f030018;
+        public static final int fragment_main=0x7f030019;
+        public static final int fragment_sign_up=0x7f03001a;
+        public static final int hc_main_layout=0x7f03001b;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
     }
     public static final class menu {
-        public static final int main=0x7f0c0000;
+        public static final int main=0x7f0d0000;
+        public static final int sign_up=0x7f0d0001;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1157,6 +1172,7 @@ containing a value of this type.
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
+        public static final int title_activity_sign_up=0x7f0a0010;
     }
     public static final class style {
         /** 
--- src/com/example/groupfinder_hci/SignUpActivity.java
+++ src/com/example/groupfinder_hci/SignUpActivity.java
@@ -4,40 +4,44 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.os.Build;
 
-public class MainActivity extends Activity {
+public class SignUpActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.fragment_main);
-		
-		Button next = (Button) findViewById(R.id.SignUp);
-		next.setOnClickListener(new View.OnClickListener() {
-		    public void onClick(View view) {
-		        Intent myIntent = new Intent( view.getContext(), SignUpActivity.class);
-		        startActivity(myIntent);
-		    }
-		});
-
+		setContentView(R.layout.fragment_sign_up);
 
 		
+		Spinner dropdown = (Spinner)findViewById(R.id.spinner);
+		String[] items = new String[]{"Undergraduate", "Graduate", "PhD"};
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items);
+		dropdown.setAdapter(adapter);
+		
+		Spinner spinner = (Spinner) findViewById(R.id.spinner1);
+		// Create an ArrayAdapter using the string array and a default spinner layout
+		ArrayAdapter<CharSequence> adap = ArrayAdapter.createFromResource(this,
+		        R.array.department_array, android.R.layout.simple_spinner_item);
+		// Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		spinner.setAdapter(adap);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.sign_up, menu);
 		return true;
 	}
 
@@ -52,6 +56,8 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+	
 
 	
 }
